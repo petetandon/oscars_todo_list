4f02d6445130b060acbbfde55ac3acc8fc293ddb
oandeliz <oandeliz@gmail.com>
2024-10-14 17:59:34 -0400

    was able to make the text field send info to the db

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,6 +13,8 @@ class _HomePageState extends State<HomePage> {
 
   final DatabaseService _databaseService = DatabaseService.instance;
 
+  String? _task = null;
+
   @override
   Widget build(Object context) {
     return  Scaffold(
@@ -28,18 +30,40 @@ class _HomePageState extends State<HomePage> {
         mainAxisSize: MainAxisSize.min,
         children: [
           TextField(
-            decoration: InputDecoration(
+            onChanged:(value) {
+              setState(() {
+                _task = value;
+              });
+    },
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
             hintText: 'Subscribe...',
             ),
           ),
-          MaterialButton(onPressed: () {},)
+          MaterialButton(
+            color: Theme.of(context).colorScheme.primary,
+        onPressed: () {
+              if (_task == null || _task == "") return;
+              _databaseService.addTask(_task!);
+              setState(() {
+                _task = null;
+              });
+              Navigator.pop(context);
+        },
+          child: const Text(
+            "Done",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+          )
+
+          ),
         ],
       ),
     ),
     );
   }, child: const Icon(Icons.add,));
 
-}
+  }
 }
 
