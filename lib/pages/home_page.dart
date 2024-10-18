594c9af27e133e497a6a483c044a9a0556574a3a
oandeliz <oandeliz@gmail.com>
2024-10-18 19:10:46 -0400

    fixed file name and implemented task model in db service

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,69 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/services/databade_services.dart';
+import 'package:todo_list/services/database_services.dart';
+
+import '../models/task.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage ({super.key});
+  const HomePage({super.key});
 
-
-    @override
-    State<HomePage> createState() => _HomePageState();
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   final DatabaseService _databaseService = DatabaseService.instance;
 
-  String? _task = null;
+  String? _task;
 
   @override
   Widget build(Object context) {
-    return  Scaffold(
+    return Scaffold(
       floatingActionButton: _addTaskButton(),
+      body: _tasksList(),
     );
   }
+
   Widget _addTaskButton() {
-  return FloatingActionButton(onPressed: () {
-    showDialog(context: context,
-      builder: (_) => AlertDialog(
-      title: const Text('Add Task'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            onChanged:(value) {
-              setState(() {
-                _task = value;
-              });
-    },
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-            hintText: 'Subscribe...',
-            ),
-          ),
-          MaterialButton(
-            color: Theme.of(context).colorScheme.primary,
+    return FloatingActionButton(
         onPressed: () {
-              if (_task == null || _task == "") return;
-              _databaseService.addTask(_task!);
-              setState(() {
-                _task = null;
-              });
-              Navigator.pop(context);
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              title: const Text('Add Task'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        _task = value;
+                      });
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Subscribe...',
+                    ),
+                  ),
+                  MaterialButton(
+                      color: Theme.of(context).colorScheme.primary,
+                      onPressed: () {
+                        if (_task == null || _task == "") return;
+                        _databaseService.addTask(_task!);
+                        setState(() {
+                          _task = null;
+                        });
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        "Done",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      )),
+                ],
+              ),
+            ),
+          );
         },
-          child: const Text(
-            "Done",
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-          )
+        child: const Icon(
+          Icons.add,
+        ));
+  }
 
-          ),
-        ],
-      ),
-    ),
-    );
-  }, child: const Icon(Icons.add,));
-
+  Widget _tasksList() {
+    return FutureBuilder(
+        future: _databaseService.getTasks(),
+        builder: (
+          context,
+          snapshot,
+        ) {
+          return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                Task task = snapshot.data![index];
+                return ListTile(
+                  title: Text(
+                    task.content,
+                  ),
+                );
+              });
+        });
   }
 }
-
--- lib/services/database_services.dart
+++ lib/services/database_services.dart
@@ -1,7 +1,8 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:todo_list/models/task.dart';
 
-class DatabaseService{
+class DatabaseService {
   static Database? _db;
   static final DatabaseService instance = DatabaseService._constructor();
 
@@ -13,18 +14,16 @@ class DatabaseService{
   DatabaseService._constructor();
 
   Future<Database> get database async {
-  if (_db != null) return _db!;
-  _db = await getDatabase();
-  return _db!;
+    if (_db != null) return _db!;
+    _db = await getDatabase();
+    return _db!;
   }
 
   Future<Database> getDatabase() async {
     final databasaDirPath = await getDatabasesPath();
     final databasePath = join(databasaDirPath, "master_dv.db");
-    final database = await openDatabase(
-        databasePath,
-      version: 1,
-      onCreate: (db, version) {
+    final database =
+        await openDatabase(databasePath, version: 1, onCreate: (db, version) {
       db.execute(''''
       CREATE TABLE _tasksTableName (
         $_tasksIdColumnName INTEGER PRIMARY KEY,
@@ -32,20 +31,29 @@ class DatabaseService{
         $_tasksStatusColumnName TEXT NOT NULL,
       )
       ''');
-      }
-    );
+    });
     return database;
   }
 
-  void addTask(String content,) async {
+  void addTask(
+    String content,
+  ) async {
     final db = await database;
     await db.insert(_tasksTableName, {
-    _tasksContentColumnName: content,
+      _tasksContentColumnName: content,
       _tasksStatusColumnName: 0,
     });
   }
-  //function to read and retrieve data
 
-
-
+  Future<List<Task>> getTasks() async {
+    final db = await database;
+    final data = await db.query(_tasksTableName);
+    return data
+        .map((e) => Task(
+              id: e["id"] as int,
+              status: e["status"] as int,
+              content: e["content"] as String,
+            ))
+        .toList();
+  }
 }
